065116319cab00fb38786b2523039e3b706f3a15
TristanAntonsen <tristan.antonsen@gmail.com>
2022-12-11 11:43:59 -0600

    day 1 part 2 solution

--- 2022_day_1/solution.py
+++ 2022_day_1/solution.py
@@ -1,17 +1,37 @@
+# # PART 1
+# with open("input.txt",'r') as file:
+#     lines = file.readlines()
+#     counter = 0
+#     last_max = 0
+#     total_elves = 0
+#     for line in lines:
+#         try:
+#             line_cals = int(line)
+#             counter += line_cals
+#         except:
+#             if counter > last_max:
+#                 last_max = counter
+#             total_elves += 1
+#             counter = 0
+
+# PART 2
 with open("input.txt",'r') as file:
     lines = file.readlines()
     counter = 0
-    last_max = 0
     total_elves = 0
+    cal_list = []
     for line in lines:
         try:
             line_cals = int(line)
             counter += line_cals
         except:
-            if counter > last_max:
-                last_max = counter
+            cal_list.append(counter)
             total_elves += 1
             counter = 0
+    top_three = sorted(cal_list)[-3:]
         
 print(f"Total elves: {total_elves}")
-print(f"Max calories: {last_max}")
+print(f"Top 3: {top_three}")
+
+# print(f"Max calories: {last_max}")
+print(f"Top three sum: {sum(top_three)}")
