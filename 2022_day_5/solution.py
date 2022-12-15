3c29412af202205a8525035fcec6acc6187d28c3
TristanAntonsen <tristan.antonsen@gmail.com>
2022-12-14 18:55:03 -0600

    day 5 part 2 solution

--- 2022_day_5/solution.py
+++ 2022_day_5/solution.py
@@ -1,3 +1,4 @@
+import time
 from pprint import pprint
 
 STACKS = [['Z', 'J', 'G'],
@@ -10,30 +11,44 @@ STACKS = [['Z', 'J', 'G'],
           ['F', 'J', 'Z', 'S'],
           ['M', 'C', 'D', 'P', 'F', 'H', 'B', 'T']]
 
+moved_stacks = STACKS
+
 def move_crate(start_stack, end_stack):
         # crate being moved
-        print(start_stack, end_stack)
         crate = start_stack[-1]
         # adding crates to new stack
         end_stack.append(crate)
         start_stack.pop(-1)
 
+def move_crates(stacks, start, end, count):
+        # crates being moved
+        start_stack = stacks[start]
+        end_stack = stacks[end]
+        crates = start_stack[-count:]
+        # adding crates to new stack
+        end_stack = end_stack + crates
+        start_stack = start_stack[:-count]
+        stacks[start] = start_stack
+        stacks[end] = end_stack
         
+def visualize_stacks(stacks):
+        
+        for i, stack in enumerate(stacks):
+                pretty_stack = ''.join(stack)
+                print(f"({i}) {pretty_stack}")
+        print("==========")
 
 with open('input.txt','r') as file:
-    lines = file.readlines()
+        lines = file.readlines()
 
-    for line in lines:
-        instructions = line.strip().split(" ")
-        move_count = int(instructions[1])
-        move_from = int(instructions[3]) - 1
-        move_to = int(instructions[5]) - 1
-        for i in range(0,move_count):
-            move_crate(STACKS[move_from], STACKS[move_to])
-
-top_crates = "".join([s[-1] for s in STACKS])
-
+        for line in lines:
+                instructions = line.strip().split(" ")
+                move_count = int(instructions[1])
+                move_from = int(instructions[3]) - 1
+                move_to = int(instructions[5]) - 1
+                # for i in range(0,move_count):
+                #     move_crate(moved_stacks[move_from], moved_stacks[move_to])
+                move_crates(moved_stacks, move_from, move_to, move_count)
+top_crates = ''.join([s[-1] for s in moved_stacks])
 print(top_crates)
-print(len(top_crates))
-
 # WFRHTFFBF wrong
